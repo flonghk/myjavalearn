7a3c6dcf08f76706a94deb1a091d2b94157d0730
huang_k <huang_k@ctrip.com>
2017-10-11 20:07:22 +0800

    --update

---
+++ src/test/java/com/hk/learn/maptest/MapTest.java
@@ -0,0 +1,16 @@
+package com.hk.learn.maptest;
+
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+
+    @Test
+    void test1()
+    {
+        Map map = new HashMap();
+
+    }
+}
